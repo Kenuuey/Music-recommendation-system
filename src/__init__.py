c7bf9b79f78075b7ac41b768390331014d92df73
gysellad <radzhabov_mg@mail.ru>
2025-09-07 16:12:47 +0300

    collaborative

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,5 @@
-from data_loader import DataLoader
-from recommenders.non_personalized import NonPersonalizedRecommender
+from .base import MusicRecommender
+from .non_personalized import NonPersonalizedRecommender
+from .collections import CollectionsRecommender
+from .collaborative import UserBasedRecommender, ItemBasedRecommender
 
-# Load data
-loader = DataLoader("./drive/MyDrive/S21/data/My_Spotify/")
-interactions = loader.load_interactions()
-lyrics = loader.load_lyrics()
-genres = loader.load_genres()
-tracks = loader.load_tracks()
-
-# Initialize recommender
-recommender = NonPersonalizedRecommender(interactions, tracks, genres)
-
-# Top 250 global
-top_250 = recommender.top_k_global(250)
-print(top_250.head())
-
-# Top 100 Rock
-top_rock = recommender.top_k_by_genre("Rock", 100)
-print(top_rock.head())
